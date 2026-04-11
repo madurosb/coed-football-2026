2ae1c82806d5cbf86b0e68915bdc8139dfae6ca0
madurosb <nirbrainin@gmail.com>
2026-04-11 17:09:11 +0300

    Add files via upload

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration - replace with your actual values from Firebase console
+const firebaseConfig = {
+  apiKey: "AIzaSyDqHBefYm759z1eR3S5V_ppaadIceeS9-4",
+  authDomain: "coed-football-2026.firebaseapp.com",
+  projectId: "coed-football-2026",
+  storageBucket: "coed-football-2026.firebasestorage.app",
+  messagingSenderId: "693898234699",
+  appId: "1:693898234699:web:a6313832e43cf7fec4b6ce",
+  measurementId: "G-NW81ETSFSE"
+};
+
+export default firebaseConfig;
